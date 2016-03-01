a0fef5f141f3f543399b142d0440d0a212e2ed8e
kmcnee <kyle.mcnee@gmail.com>
2016-03-01 12:16:37 -0500

    second commit

--- APIandGsonLab/app/src/main/java/com/example/kylemcnee/apiandgsonlab/MainActivity.java
+++ APIandGsonLab/app/src/main/java/com/example/kylemcnee/apiandgsonlab/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.kylemcnee.apiandgsonlab;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    String mQuery;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class SearchAsyncTask extends AsyncTask<String, Void, Void>{
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private String getInputData(InputStream inStream) throws IOException{
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+
+        String character;
+
+        while ((character = reader.readLine()) != null){
+            builder.append(character);
+        }
+
+        return builder.toString();
     }
 
 
