c90b4fd65cf47655ed6c22b61273a94da482c861
Boloutare Doubeni <tarecd@gmail.com>
2016-03-01 13:05:43 -0500

    Initial Commit

---
+++ PokeDexApp/app/src/main/java/com/boloutaredoubeni/pokedex/MainActivity.java
@@ -0,0 +1,18 @@
+package com.boloutaredoubeni.pokedex;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+  private ListView mListView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+  }
+}
